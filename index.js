573dcd8ffcf58c94b57bb3623192e7a3dac7df79
sainioan <sainio.anniina@gmail.com>
2020-05-24 15:53:45 +0300

    api/persons

---
+++ index.js
@@ -0,0 +1,61 @@
+
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+       
+        content: 
+        {name : "Arto Hellas", 
+        number: "040-123456"},
+        id: 1,
+       // date: new Date(),
+      //  important: true
+      },
+        
+      { 
+      
+        content: {
+        name : "Ada Lovelace", 
+        number: "39-44-5323523",
+        },
+        id: 2
+       // date: new Date(),
+       // important: true
+      },
+        
+    
+     { 
+       
+        content: {
+        name :  "Dan Abramov", 
+        number: "12-43-234345",
+        },
+        id: 3,
+       // date: new Date(),
+       // important: true 
+      },
+      { 
+      
+        content: {
+        name :  "Mary Poppendieck", 
+        number: "39-23-6423122",
+        },
+        id: 4,
+       // date: new Date(),
+       // important: true
+     },
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
