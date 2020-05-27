bf033ba3d77f3c37d8cd696f724b9184f108badf
sainioan <sainio.anniina@gmail.com>
2020-05-27 14:39:43 +0300

    update

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 } */
 
-app.use(errorHandler)
+// app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
