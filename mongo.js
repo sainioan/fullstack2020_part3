818e2124878574e7f6a0d077d732e65402e3734b
sainioan <sainio.anniina@gmail.com>
2020-05-28 07:37:03 +0300

    update

--- mongo.js
+++ mongo.js
@@ -7,11 +7,11 @@ if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
-const password = 'inChrist1'
-//const password = process.argv[2]
+//const password = 'inChrist1
+const password = process.argv[2]
 
 // const url = process.env.MONGODB_URI
-const url = 'MONGODB_URI=mongodb+srv://anniinasainio:inChrist1@cluster0-wfn7g.mongodb.net/test?retryWrites=true&w=majority'
+const url = `MONGODB_URI=mongodb+srv://anniinasainio:${password}@cluster0-wfn7g.mongodb.net/test?retryWrites=true&w=majority`
 //mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect(url)
 //mongoose.connect(url, { useNewUrlParser: true })
