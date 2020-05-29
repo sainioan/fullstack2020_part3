0aa8b5d55d8ab737400dccd9deb38124c5acbf63
sainioan <sainio.anniina@gmail.com>
2020-05-29 10:36:40 +0300

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "indent": [
+      "error",
+      2
+  ],
+  'linebreak-style': [
+      'error',
+      'unix'
+  ],
+  'quotes': [
+      'error',
+      'single'
+  ],
+  'semi': [
+      'error',
+      'never'
+  ]
+  },
 };
