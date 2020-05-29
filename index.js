b148b08bd456613c401ccdef793d128bf994a2b6
sainioan <sainio.anniina@gmail.com>
2020-05-29 09:03:19 +0300

    update

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  //  console.error(error.message)
+    console.error(error.message)
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
